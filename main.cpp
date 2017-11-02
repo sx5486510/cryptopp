e8d2a4f9c789c819d98022bda27274ca3da9fd8d
sx5486510 <sx5486510@126.com>
2017-11-02 10:53:32 +0800

    1

--- main.cpp
+++ main.cpp
@@ -64,8 +64,45 @@ void ECB_AESDecryptStr(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	);
 }
 
+void ECB_AESEncryptStr(const Nan::FunctionCallbackInfo<v8::Value>& info)
+{
+	Local<Object> input = info[0]->ToObject();
+	Local<Object> input2 = info[1]->ToObject();
+
+	if (!Buffer::HasInstance(input))
+		return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
+
+	if (!Buffer::HasInstance(input2))
+		return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
+
+	std::string sKey = std::string(Buffer::Data(input), Buffer::Length(input));
+	std::string plainText = std::string(Buffer::Data(input2), Buffer::Length(input2));
+
+	std::string outstr;
+	//Ìîkey  
+	SecByteBlock key(AES::MAX_KEYLENGTH);
+	memset(key, 0x30, key.size());
+	if (sKey.size() <= AES::MAX_KEYLENGTH)
+		memcpy(key, sKey.c_str(), sKey.size());
+	else
+		memcpy(key, sKey.c_str(), AES::MAX_KEYLENGTH);
+
+	AES::Encryption aesEncryption((byte *)key, AES::MAX_KEYLENGTH);
+
+	ECB_Mode_ExternalCipher::Encryption ecbEncryption(aesEncryption);
+	StreamTransformationFilter ecbEncryptor(ecbEncryption, new HexEncoder(new StringSink(outstr)));
+	ecbEncryptor.Put((byte *)plainText.c_str(), plainText.length());
+	ecbEncryptor.MessageEnd();
+
+	v8::Local<v8::Value> returnValue = Nan::CopyBuffer((char*)outstr.data(), outstr.size()).ToLocalChecked();
+	info.GetReturnValue().Set(
+		returnValue
+	);
+}
+
 NAN_MODULE_INIT(init) {
 	Nan::Set(target, Nan::New("ECB_AESDecryptStr").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(ECB_AESDecryptStr)).ToLocalChecked());
+	Nan::Set(target, Nan::New("ECB_AESEncryptStr").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(ECB_AESEncryptStr)).ToLocalChecked());
 }
 
 NODE_MODULE(ECB_AESDecryptStr, init)
